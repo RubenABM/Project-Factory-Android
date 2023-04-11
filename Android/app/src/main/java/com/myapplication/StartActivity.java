3a3d8b2470db0fbe0f9cc8a55164e629d07d0bf9
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2023-04-11 02:47:07 +0100

    .

--- Android/app/src/main/java/com/myapplication/StartActivity.java
+++ Android/app/src/main/java/com/myapplication/StartActivity.java
@@ -1,17 +1,95 @@
 package com.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 public class StartActivity extends AppCompatActivity {
 
+    Button startbtn;
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         Fragment fragment = new MapFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
+
+        drawer = findViewById(R.id.drawer_layout);
+        startbtn = findViewById(R.id.start_btn);
+
+    }
+
+    //menus
+    public void OpenLeftSideMenu(View view){openDrawer(drawer);}
+    public void OpenRightSideMenu(View view){openDrawer2(drawer);}
+    public void OpenTripDetailsDrawer(View view){openDrawer3(drawer);}
+    public static void openDrawer(DrawerLayout drawer) {
+        drawer.openDrawer(GravityCompat.START);
+    }
+
+    public static void openDrawer2(DrawerLayout drawer) {
+        drawer.openDrawer(GravityCompat.END);
+    }
+    public static void openDrawer3(DrawerLayout drawer) {
+        drawer.openDrawer(GravityCompat.END);
+    }
+    public static void closeDrawer(DrawerLayout drawer) {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+    }
+    public void ClickHelmets(View view){goToActivity(this, HelmetsActivity.class);}
+    public void ClickActivity(View view){
+        //goToActivity(this,**);
+        Toast.makeText(this, "Function 'Activities' is not available yet", Toast.LENGTH_SHORT).show();
+    }
+    public void ClickChallenges(View view){goToActivity(this, ChallengesActivity.class);}
+    public void ClickHealth(View view){
+        //goToActivity(this,**);
+        Toast.makeText(this, "Function 'Health' is not available yet", Toast.LENGTH_SHORT).show();
+    }
+    public void ClickPoints(View view){
+        //goToActivity(this,**);
+        Toast.makeText(this, "Function 'Points' is not available yet", Toast.LENGTH_SHORT).show();
+    }
+    public void ClickProfile(View view){
+        //goToActivity(this,**);
+        Toast.makeText(this, "Function 'Profile' is not available yet", Toast.LENGTH_SHORT).show();
+    }
+    public void ClickSubscription(View view){goToActivity(this, SubscriptionActivity.class);}
+    public void ClickSettings(View view){
+        //goToActivity(this,**);
+        Toast.makeText(this, "Function 'Settings' is not available yet", Toast.LENGTH_SHORT).show();
+    }
+    public void ClickLogout(View view){
+        //goToActivity(this,**);
+        Toast.makeText(this, "Function 'Logout' is not available yet", Toast.LENGTH_SHORT).show();
+    }
+    public static void goToActivity(Activity activity, Class aClass) {
+        Intent intent = new Intent(activity, aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawer);
     }
 }
