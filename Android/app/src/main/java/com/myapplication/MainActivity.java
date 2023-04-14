198cfbd5baa6e30ac8f6b5544f0dd72e57b40965
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2023-04-14 09:51:52 +0100

    server config test

--- Android/app/src/main/java/com/myapplication/MainActivity.java
+++ Android/app/src/main/java/com/myapplication/MainActivity.java
@@ -1,25 +1,104 @@
 package com.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.format.Formatter;
 import android.util.Log;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileWriter;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "HTTPDIR";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String dir = getApplicationInfo().dataDir;
-        Log.d("HTTP", dir);
-        //TinyWebServer.startServer("localhost",9000, dir);
+        //String dir = getApplicationInfo().dataDir;
+
+        if (!checkPermissions()) {
+            requestPermissions();
+        }
+
+        if (!checkPermissions()) {
+            Log.d(TAG, "Não foram dadas permissões");
+            finish();
+        } else {
+            Log.d(TAG, "Já há permissões de escrita");
+        }
+
+        File filesDir = getFilesDir();
+        String dir = filesDir.getAbsolutePath();
+        Log.d(TAG, dir);
+
+        Context context = getApplicationContext();
+        WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        @SuppressWarnings("deprecation")
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        Log.d(TAG, "IP: " + ip);
+
+        TextView txt = (TextView) findViewById(R.id.texto);
+        txt.setText("IP: " + ip);
+
+        criaIndex(filesDir, ip);
+
+        //TinyWebServer.startServer("localhost",8080, dir);
+        TinyWebServer.startServer(ip,8080, dir);
     }
 
     @Override
     public void onDestroy(){
         super.onDestroy();
         //stop webserver on destroy of service or process
-        //TinyWebServer.stopServer();
+        TinyWebServer.stopServer();
+    }
+
+    private boolean checkPermissions() {
+        Log.d(TAG, "checkPermissions");
+        boolean perm = PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) &&
+                PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                ;
+        return  perm;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                },
+                1);
+    }
+
+    public void criaIndex(File filesDir, String ip) {
+        try {
+            File f = new File(filesDir + "/index.html");
+
+            if (f.exists()) {
+                Log.d(TAG, "Ficheiro index.html existe");
+                f.delete();
+                Log.d(TAG, "Vou abrir ficheiro para escrita");
+                FileWriter out = new FileWriter(f, true);
+                Log.d(TAG, "Abri ficheiro para escrita");
+                out.append("<html><head><meta charset='utf-8'></head><body><h2>Android</h2><p>O meu IP é: " + ip + "</p></body></html>");
+                Log.d(TAG, "Escrevi no ficheiro");
+                out.flush();
+                out.close();
+            }
+
+        } catch(Exception e){
+            e.printStackTrace();
+            Log.d(TAG, "Erro a criar o ficheiro: " + e.toString());
+        }
     }
 }
