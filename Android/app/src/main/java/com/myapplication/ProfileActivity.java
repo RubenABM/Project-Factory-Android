39c4ab41e3560c662e1f1627b6ce6e685c75e395
martamsrs <91379645+martamsrs@users.noreply.github.com>
2023-04-20 18:25:19 +0100

    profile update

--- Android/app/src/main/java/com/myapplication/ProfileActivity.java
+++ Android/app/src/main/java/com/myapplication/ProfileActivity.java
@@ -3,12 +3,34 @@ package com.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    EditText nameBox, mailBox, healthBox, pointBox;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        nameBox=findViewById(R.id.Editname);
+        mailBox=findViewById(R.id.Editmail);
+        healthBox=findViewById(R.id.EditHealth);
+        pointBox=findViewById(R.id.EditPoint);
     }
+        public void EditProfile(View view){
+
+
+        }
+
+
+
+
+
+
+
 }
