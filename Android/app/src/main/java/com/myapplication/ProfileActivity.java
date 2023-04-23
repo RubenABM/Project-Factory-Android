24dd432e1fc02272825420b3bca8cc556ef2709a
martamsrs <91379645+martamsrs@users.noreply.github.com>
2023-04-23 18:01:49 +0100

    profile update

--- Android/app/src/main/java/com/myapplication/ProfileActivity.java
+++ Android/app/src/main/java/com/myapplication/ProfileActivity.java
@@ -1,15 +1,18 @@
 package com.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
     EditText nameBox, mailBox, healthBox, pointBox;
+    DrawerLayout drawerLayout;
 
 
     @Override
@@ -17,12 +20,27 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        drawerLayout = findViewById(R.id.imageView3);
         nameBox=findViewById(R.id.Editname);
         mailBox=findViewById(R.id.Editmail);
         healthBox=findViewById(R.id.EditHealth);
         pointBox=findViewById(R.id.EditPoint);
     }
-        public void EditProfile(View view){
+        public void updateProfile(View view)
+        {
+            String name = nameBox.getText().toString();
+            String email = mailBox.getText().toString();
+            String health = healthBox.getText().toString();
+            String point = pointBox.getText().toString();
+
+            if(email.isEmpty() || name.isEmpty() || health.isEmpty())
+            {
+                Toast.makeText(getBaseContext(), "Preencher todos os campos!", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+
+
 
 
         }
