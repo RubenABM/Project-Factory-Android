1825b822b2db47ac90fe19fff83d9db212950312
Ana Bernardino <bernardino.ana490@hotmail.com>
2023-04-24 23:32:20 +0100

    criar variaveis nas activities

--- Android/app/src/main/java/com/myapplication/HealthActivity.java
+++ Android/app/src/main/java/com/myapplication/HealthActivity.java
@@ -3,12 +3,19 @@ package com.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HealthActivity extends AppCompatActivity {
+
+    TextView bpmRes;
+    TextView tempRes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
+
+        bpmRes = findViewById(R.id.textViewBPMResults);
+        tempRes = findViewById(R.id.textViewTemperatureResults);
     }
 }
--- Android/app/src/main/java/com/myapplication/SettingsActivity.java
+++ Android/app/src/main/java/com/myapplication/SettingsActivity.java
@@ -3,12 +3,17 @@ package com.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    Switch darkModeSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        darkModeSwitch = findViewById(R.id.switchMode);
     }
 }
