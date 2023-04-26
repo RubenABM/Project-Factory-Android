19bfc8586d30039f57679e487ba4c0306a8eb3e8
martamsrs <91379645+martamsrs@users.noreply.github.com>
2023-04-26 11:43:12 +0100

    dados get

--- Android/app/src/main/java/com/myapplication/ProfileActivity.java
+++ Android/app/src/main/java/com/myapplication/ProfileActivity.java
@@ -9,14 +9,21 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.myapplication.downloadtasks.JSONObj;
+import com.myapplication.downloadtasks.PostMethod;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    EditText nameBox, mailBox, healthBox, pointBox, passBox, pass2Box;
+    EditText nameBox, mailBox, healthBox, pointBox, pointBox2, passBox, pass2Box;
     DrawerLayout drawerLayout;
     JSONObject loginjson = null;
+    static String iduser;
 
 
     @Override
@@ -48,24 +55,42 @@ public class ProfileActivity extends AppCompatActivity {
                 Toast.makeText(getBaseContext(), "As palavras passes não coincidem!", Toast.LENGTH_LONG).show();
                 return;
             }
-            Toast.makeText(this,"Data changed! ", Toast.LENGTH_LONG).show();
 
-            // Dados do get
+
+            // Dados do get dos pontos
 
             iduser = getIntent().getStringExtra("key");
             JSONObj task = new JSONObj();
             try {
-                loginjson = task.execute("https://myiade.herokuapp.com/api/students/"+iduser).get();
-                username.setText(loginjson.getString("name"));
-                useremail.setText(loginjson.getString("email"));
+                loginjson = task.execute("http://13.40.214.190:5000/users/"+1).get();
+                pointBox2.setText(loginjson.getString("user_points"));
 
-
-
-
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+                loginjson = null;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                loginjson = null;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
 
 
         }
+
+            //Dados do post do nome, mail e pass
+
+            //Map<String, String> postData = new HashMap<>();
+            //postData.put("user_name", name);
+            //postData.put("user_email", email);
+            //postData.put("user_password", pass);
+
+            // Post call
+            //PostMethod task = new PostMethod(postData);
+            //task.execute("http://13.40.214.190:5000/users/insertnewuser");
+
+            //Toast.makeText(this,"Dados alterados com sucesso!", Toast.LENGTH_SHORT).show();
 
 
 
