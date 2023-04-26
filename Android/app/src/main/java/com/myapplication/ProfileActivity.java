5836f2804986cd6f0573991e454d470a11437bf5
martamsrs <91379645+martamsrs@users.noreply.github.com>
2023-04-26 12:42:10 +0100

    o get funciona!

--- Android/app/src/main/java/com/myapplication/ProfileActivity.java
+++ Android/app/src/main/java/com/myapplication/ProfileActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.myapplication.downloadtasks.JSONObj;
+import com.myapplication.downloadtasks.JSONObjToArray;
 import com.myapplication.downloadtasks.PostMethod;
 
 import org.json.JSONException;
@@ -20,82 +23,70 @@ import java.util.concurrent.ExecutionException;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    EditText nameBox, mailBox, healthBox, pointBox, pointBox2, passBox, pass2Box;
+    EditText nameBox, mailBox, healthBox, pointBox, passBox, pass2Box;
+    TextView points;
     DrawerLayout drawerLayout;
-    JSONObject loginjson = null;
-    static String iduser;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        JSONObject loginjson = null;
+
+        nameBox = findViewById(R.id.Editname);
+        mailBox = findViewById(R.id.Editmail);
+        passBox = findViewById(R.id.EditPass);
+        pass2Box = findViewById(R.id.EditPass2);
+        points = findViewById(R.id.pointBox2);
+
+        String name = nameBox.getText().toString();
+        String email = mailBox.getText().toString();
+        String pass = passBox.getText().toString();
+        String pass2 = pass2Box.getText().toString();
+
+        /*
+        if (email.isEmpty() || name.isEmpty() || pass.isEmpty()) {
+            Toast.makeText(getBaseContext(), "Preencher todos os campos!", Toast.LENGTH_LONG).show();
+        }
+        if (!pass.equals(pass2)) {
+            Toast.makeText(getBaseContext(), "As palavras passes não coincidem!", Toast.LENGTH_LONG).show();
+        }*/
 
 
-        nameBox=findViewById(R.id.Editname);
-        mailBox=findViewById(R.id.Editmail);
-        passBox=findViewById(R.id.EditPass);
-        pass2Box=findViewById(R.id.EditPass2);
+        // Dados do get dos pontos
 
-    }
-        public void updateProfile(View view)
-        {
-            String name = nameBox.getText().toString();
-            String email = mailBox.getText().toString();
-            String pass = passBox.getText().toString();
-            String pass2 = pass2Box.getText().toString();
+        //iduser = getIntent().getStringExtra("key");
+        //JSONObj task = new JSONObj();
+        JSONObjToArray task = new JSONObjToArray();
+        try {
+            loginjson = task.execute("http://13.40.214.190:5000/users/1").get();
+            points.setText(loginjson.getString("user_points"));
 
-            if(email.isEmpty() || name.isEmpty() || pass.isEmpty())
-            {
-                Toast.makeText(getBaseContext(), "Preencher todos os campos!", Toast.LENGTH_LONG).show();
-                return;
-            }
-            if(!pass.equals(pass2))
-            {
-                Toast.makeText(getBaseContext(), "As palavras passes não coincidem!", Toast.LENGTH_LONG).show();
-                return;
-            }
-
-
-            // Dados do get dos pontos
-
-            iduser = getIntent().getStringExtra("key");
-            JSONObj task = new JSONObj();
-            try {
-                loginjson = task.execute("http://13.40.214.190:5000/users/"+1).get();
-                pointBox2.setText(loginjson.getString("user_points"));
-
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-                loginjson = null;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                loginjson = null;
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-
-
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            loginjson = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            loginjson = null;
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
-            //Dados do post do nome, mail e pass
 
-            //Map<String, String> postData = new HashMap<>();
-            //postData.put("user_name", name);
-            //postData.put("user_email", email);
-            //postData.put("user_password", pass);
+    }
 
-            // Post call
-            //PostMethod task = new PostMethod(postData);
-            //task.execute("http://13.40.214.190:5000/users/insertnewuser");
+    //Dados do post do nome, mail e pass
 
-            //Toast.makeText(this,"Dados alterados com sucesso!", Toast.LENGTH_SHORT).show();
+    //Map<String, String> postData = new HashMap<>();
+    //postData.put("user_name", name);
+    //postData.put("user_email", email);
+    //postData.put("user_password", pass);
 
+    // Post call
+    //PostMethod task = new PostMethod(postData);
+    //task.execute("http://13.40.214.190:5000/users/insertnewuser");
 
-
-
-
-
-
+    //Toast.makeText(this,"Dados alterados com sucesso!", Toast.LENGTH_SHORT).show();
 }
