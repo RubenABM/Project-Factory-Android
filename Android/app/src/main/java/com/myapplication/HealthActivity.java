8ceb4c1b6088f03c66de119966b3d71c2c7f4528
Ana Bernardino <bernardino.ana490@hotmail.com>
2023-04-27 11:58:55 +0100

    tentativa ir buscar dados health

--- Android/app/src/main/java/com/myapplication/HealthActivity.java
+++ Android/app/src/main/java/com/myapplication/HealthActivity.java
@@ -5,17 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.myapplication.downloadtasks.JSONObj;
+import com.myapplication.downloadtasks.JSONObjToArray;
+
+import org.json.JSONObject;
+
 public class HealthActivity extends AppCompatActivity {
 
     TextView bpmRes;
     TextView tempRes;
+    static String iduser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
+        JSONObject loginjson = null;
 
         bpmRes = findViewById(R.id.textViewBPMResults);
         tempRes = findViewById(R.id.textViewTemperatureResults);
+
+        //Método GET para ir buscar a Health Data do user
+
+        iduser = getIntent().getStringExtra("key");
+        JSONObjToArray task = new JSONObjToArray();
+
+        try{
+            //loginjson = task.execute("http://13.40.214.190:5000/users/" + iduser + idrota).get();
+            //bpmRes.setText(loginjson.getString("user_"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
