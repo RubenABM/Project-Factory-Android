7173703932de7a082b5d6d8469c9113d847daba4
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2023-05-22 11:22:00 +0100

    .

--- Android/app/src/main/java/com/myapplication/TinyWebServer.java
+++ Android/app/src/main/java/com/myapplication/TinyWebServer.java
@@ -256,13 +256,16 @@ public class TinyWebServer extends Thread {
 
         String data = "";
         switch (location) {
+
             case "/":
                 //root location, server index file
                 CONTENT_TYPE = "text/html";
                 data=readFile(WEB_DIR_PATH+"/"+INDEX_FILE_NAME);
                 constructHeader(out, data.length() + "", data);
                 break;
-            default:
+
+
+            case "/test":
 
                 System.out.println("url location -> " + location);
                 URL geturl = getDecodedUrl("http://localhost" + location);
@@ -274,6 +277,9 @@ public class TinyWebServer extends Thread {
                     if(REQUEST_TYPE.equals("POST")){
                         if (qparms==null){ qparms=new HashMap<String,String>();}
                         qparms.put("_POST", postData);
+                        Log.d(TAG, "postData: ");
+                        Log.d(TAG, postData);
+                        
                     }
                     //System.out.println("File name " + fileName);
                     //System.out.println("url parms " + qparms);
