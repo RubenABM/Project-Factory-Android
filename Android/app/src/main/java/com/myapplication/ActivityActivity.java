1b5d1603245f52abef14f7fa811d0660f79cbdd7
Ana Bernardino <bernardino.ana490@hotmail.com>
2023-05-28 17:21:40 +0100

    recuperação de ficheiros

--- Android/app/src/main/java/com/myapplication/ActivityActivity.java
+++ Android/app/src/main/java/com/myapplication/ActivityActivity.java
@@ -7,12 +7,18 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import static com.myapplication.MapFragment.firstMap;
 import static com.myapplication.StartActivity.startingflag;
+
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -52,78 +58,173 @@ public class ActivityActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity);
         iduser = getIntent().getStringExtra("key");
-        String firstURL = "http://35.176.222.11:5000/routes/user/" + iduser;
+        String firstURL = "http://35.176.222.11:5000/routes/user/" + 2;
         drawer = findViewById(R.id.drawer_layout);
-        atualizar = findViewById(R.id.editButton);
+
+        LinearLayout ll = findViewById(R.id.linearActivityActivity);
 
         JSONArray getjson = null;
         JSONArr taskget = new JSONArr();
-        JSONObject test = null;
         try {
             getjson = taskget.execute(firstURL).get();
             Log.d(TAG, getjson.toString());
-            for (int i = 0; i < getjson.length(); i++){
-                test = getjson.getJSONObject(i);
-                routeCards = findViewById(R.id.routeCard);
-                idroute = test.getString("route_id");
-                routeCoords = test.getString("route_coord");
-                Log.d(TAG, routeCoords);
-                routes.put(idroute, routeCoords);
-                startDnTime = findViewById(R.id.dateNTimeStart);
-                //String tripTime = getjson.getString("data_startTime") + " - " + getjson.getString("data_endTime");
-                String tripTime = "GET fix!";
-                startDnTime.setText(tripTime);
-                idrout = findViewById(R.id.routeid);
-                idrout.setText(idroute);
-                routeName = findViewById(R.id.editTextPercurso1);
-                routeName.setText(test.getString("route_name"));
-                iconTrip = findViewById(R.id.imageView10);
+            for (int i = 0; i < getjson.length(); i++) {
+                JSONObject test = getjson.getJSONObject(i);
+
+                RelativeLayout rl = new RelativeLayout(getBaseContext());
+                rl.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                RelativeLayout rl2 = new RelativeLayout(getBaseContext());
+                RelativeLayout.LayoutParams rl2Params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                rl2Params.setMargins(convertDpToPixel(10, getBaseContext()), convertDpToPixel(10, getBaseContext()),0,0);
+                rl2.setLayoutParams(rl2Params);
+                rl.addView(rl2);
+
+                ImageView img = new ImageView(getBaseContext());
+                img.setImageDrawable(getDrawable(R.drawable.edit_road));
+                img.setLayoutParams(new RelativeLayout.LayoutParams(convertDpToPixel(77, getBaseContext()), convertDpToPixel(58, getBaseContext())));
+                img.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            GoToTrip(v, test.getString("route_id"));
+                        }catch(Exception e){}
+                    }
+                });
+                rl2.addView(img);
+
+                Button btn = new Button(getBaseContext());
+                RelativeLayout.LayoutParams params8 = new RelativeLayout.LayoutParams(convertDpToPixel(75, getBaseContext()),convertDpToPixel(48, getBaseContext()));
+                params8.setMargins(0, convertDpToPixel(60, getBaseContext()), 0, 0);
+                params8.addRule(RelativeLayout.BELOW, img.getId());
+                btn.setBackgroundColor(Color.parseColor("#003B3F"));
+                btn.setTextColor(Color.parseColor("#FFFFFF"));
+                btn.setText("Edit");
+                btn.setLayoutParams(params8);
+                rl2.addView(btn);
+
+
+                RelativeLayout rl3 = new RelativeLayout(getBaseContext());
+                RelativeLayout.LayoutParams rl3params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                rl3params.setMargins(convertDpToPixel(100, getBaseContext()), convertDpToPixel(10, getBaseContext()), 0, 0);
+                rl3.setLayoutParams(rl3params);
+                rl.addView(rl3);
+
+                TextView textRouteId = new TextView(getBaseContext());
+                RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                textRouteId.setLayoutParams(params2);
+                textRouteId.setText("Route ID: ");
+                textRouteId.setTextColor(Color.parseColor("#000000"));
+                textRouteId.setTextSize(20);
+                rl3.addView(textRouteId);
+
+                TextView textRouteIdPlaceHolder = new TextView(getBaseContext());
+                RelativeLayout.LayoutParams params3 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                params3.setMargins(convertDpToPixel(85, getBaseContext()), 0, 0, 0);
+                params3.addRule(RelativeLayout.END_OF, textRouteId.getId());
+                textRouteIdPlaceHolder.setLayoutParams(params3);
+                textRouteIdPlaceHolder.setText(test.getString("route_id"));
+                textRouteIdPlaceHolder.setTextSize(20);
+                textRouteIdPlaceHolder.setTextColor(Color.parseColor("#000000"));
+                rl3.addView(textRouteIdPlaceHolder);
+
+                TextView textPercursoName = new TextView(getBaseContext());
+                RelativeLayout.LayoutParams params4 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                params4.setMargins(0, convertDpToPixel(30, getBaseContext()), 0, 0);
+                params4.addRule(RelativeLayout.BELOW, textRouteId.getId());
+                textPercursoName.setLayoutParams(params4);
+                textPercursoName.setText("Name: ");
+                textPercursoName.setTextColor(Color.parseColor("#000000"));
+                textPercursoName.setTextSize(20);
+                rl3.addView(textPercursoName);
+
+                EditText editTextPercursoName = new EditText(getBaseContext());
+                RelativeLayout.LayoutParams params5 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                params5.setMargins(convertDpToPixel(65, getBaseContext()), convertDpToPixel(20, getBaseContext()), 0, 0);
+                params5.addRule(RelativeLayout.BELOW, textRouteIdPlaceHolder.getId());
+                editTextPercursoName.setLayoutParams(params5);
+                editTextPercursoName.setText(test.getString("route_name"));
+                editTextPercursoName.setTextColor(Color.parseColor("#000000"));
+                editTextPercursoName.setTextSize(20);
+                rl3.addView(editTextPercursoName);
+
+                TextView textDate = new TextView(getBaseContext());
+                RelativeLayout.LayoutParams params6 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                params6.addRule(RelativeLayout.BELOW, textPercursoName.getId());
+                params6.setMargins(0, convertDpToPixel(60, getBaseContext()), 0, 0);
+                textDate.setLayoutParams(params6);
+                textDate.setText("Date: ");
+                textDate.setTextColor(Color.parseColor("#000000"));
+                textDate.setTextSize(20);
+                rl3.addView(textDate);
+
+                TextView textDatePlaceHolder = new TextView(getBaseContext());
+                RelativeLayout.LayoutParams params7 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                params7.setMargins(convertDpToPixel(70, getBaseContext()), convertDpToPixel(60, getBaseContext()), 0, 0);
+                params7.addRule(RelativeLayout.BELOW, editTextPercursoName.getId());
+                textDatePlaceHolder.setLayoutParams(params7);
+                textDatePlaceHolder.setText(test.getString("route_id"));
+                textDatePlaceHolder.setTextColor(Color.parseColor("#000000"));
+                textDatePlaceHolder.setTextSize(20);
+                rl3.addView(textDatePlaceHolder);
+
+                routes.put(test.getString("route_id"), test.getString("route_coord"));
+
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            UpdateText(v, test.getString("route_id"), editTextPercursoName);
+                        }catch(Exception e) {}
+                    }
+                });
+
+                ll.addView(rl);
             }
 
         } catch (ExecutionException | InterruptedException | JSONException e) {
             e.printStackTrace();
         }
-
-        iconTrip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GoToTrip(v, idroute);
-            }
-        });
-
-        atualizar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UpdateText(v,idrout.toString());
-            }
-        });
-
     }
 
-    public void UpdateText(View v, String routeId){
+    public void UpdateText(View v, String routeId, EditText routeNameEdit) {
 
         try {
             Map<String, String> postData = new HashMap<>();
-            postData.put("route_name", routeName.getText().toString());
+            postData.put("route_name", routeNameEdit.getEditableText().toString());
             PostMethod task = new PostMethod(postData);
             String url = "http://35.176.222.11:5000/routes/updateroutename/" + iduser + "/" + routeId;
             task.execute(url);
 
-            Toast.makeText(this,"Dados alterados com sucesso!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Dados alterados com sucesso!", Toast.LENGTH_SHORT).show();
 
-        } catch (Exception e){
-            Toast.makeText(this,"ERRO!", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "ERRO!", Toast.LENGTH_SHORT).show();
         }
 
     }
 
     //menus
-    public void OpenLeftSideMenu(View view){openDrawer(drawer);}
-    public void OpenRightSideMenu(View view){openDrawer2(drawer);}
+    public void OpenLeftSideMenu(View view) {
+        openDrawer(drawer);
+    }
 
-    public void CloseLeftMenu(View view){closeDrawer(drawer);}
-    public void CloseRightMenu(View view){closeDrawer2(drawer);}
-    public void OpenTripDetailsDrawer(View view){openDrawer3(drawer);}
+    public void OpenRightSideMenu(View view) {
+        openDrawer2(drawer);
+    }
+
+    public void CloseLeftMenu(View view) {
+        closeDrawer(drawer);
+    }
+
+    public void CloseRightMenu(View view) {
+        closeDrawer2(drawer);
+    }
+
+    public void OpenTripDetailsDrawer(View view) {
+        openDrawer3(drawer);
+    }
+
     public static void openDrawer(DrawerLayout drawer) {
         drawer.openDrawer(GravityCompat.START);
     }
@@ -131,14 +232,17 @@ public class ActivityActivity extends AppCompatActivity {
     public static void openDrawer2(DrawerLayout drawer) {
         drawer.openDrawer(GravityCompat.END);
     }
+
     public static void openDrawer3(DrawerLayout drawer) {
         drawer.openDrawer(GravityCompat.END);
     }
+
     public static void closeDrawer(DrawerLayout drawer) {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         }
     }
+
     public static void closeDrawer2(DrawerLayout drawer) {
         if (drawer.isDrawerOpen(GravityCompat.END)) {
             drawer.closeDrawer(GravityCompat.END);
@@ -146,15 +250,39 @@ public class ActivityActivity extends AppCompatActivity {
     }
 
 
-    public void ClickHelmets(View view){StartActivity.goToActivity(this, HelmetsActivity.class);}
-    public void ClickActivity(View view){StartActivity.goToActivity2(this, ActivityActivity.class, iduser);}
-    public void ClickChallenges(View view){StartActivity.goToActivity(this, ChallengesActivity.class);}
-    public void ClickHealth(View view){StartActivity.goToActivity(this, HealthActivity.class);}
-    public void ClickPoints(View view){StartActivity.goToActivity(this, PointsActivity.class);}
-    public void ClickProfile(View view){StartActivity.goToActivity(this, ProfileActivity.class);}
-    public void ClickSubscription(View view){StartActivity.goToActivity(this, SubscriptionActivity.class);}
-    public void ClickSettings(View view){StartActivity.goToActivity(this, SettingsActivity.class);}
-    public void ClickLogout(View view){
+    public void ClickHelmets(View view) {
+        StartActivity.goToActivity(this, HelmetsActivity.class);
+    }
+
+    public void ClickActivity(View view) {
+        StartActivity.goToActivity2(this, ActivityActivity.class, iduser);
+    }
+
+    public void ClickChallenges(View view) {
+        StartActivity.goToActivity(this, ChallengesActivity.class);
+    }
+
+    public void ClickHealth(View view) {
+        StartActivity.goToActivity(this, HealthActivity.class);
+    }
+
+    public void ClickPoints(View view) {
+        StartActivity.goToActivity(this, PointsActivity.class);
+    }
+
+    public void ClickProfile(View view) {
+        StartActivity.goToActivity(this, ProfileActivity.class);
+    }
+
+    public void ClickSubscription(View view) {
+        StartActivity.goToActivity(this, SubscriptionActivity.class);
+    }
+
+    public void ClickSettings(View view) {
+        StartActivity.goToActivity(this, SettingsActivity.class);
+    }
+
+    public void ClickLogout(View view) {
         //goToActivity(this,**);
         Toast.makeText(this, "Function 'Logout' is not available yet", Toast.LENGTH_SHORT).show();
     }
@@ -178,4 +306,7 @@ public class ActivityActivity extends AppCompatActivity {
         StartActivity.goToActivity(this, StartActivity.class);
     }
 
+    public static int convertDpToPixel(int dp, Context context){
+        return dp * ((int) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+    }
 }
--- Android/app/src/main/java/com/myapplication/ChallengesActivity.java
+++ Android/app/src/main/java/com/myapplication/ChallengesActivity.java
@@ -2,13 +2,29 @@ package com.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.myapplication.downloadtasks.DownloadTask;
+import com.myapplication.downloadtasks.JSONObjToArray;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class ChallengesActivity extends AppCompatActivity {
 
+    TextView textChalDesc, textPoints;
     ProgressBar progressBar;
     static String iduser;
 
@@ -17,14 +33,71 @@ public class ChallengesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenges);
 
-        JSONObject challengesjson = null;
-        progressBar = findViewById(R.id.progressBarChallenge);
-        // set the progress value based on the challenge progress
-        //int progress =
+        textChalDesc = findViewById(R.id.textViewChallengeDescription);
+        textPoints = findViewById(R.id.textViewChallengePoints);
 
-        //código challenge bar
-        //progressBar.setProgress(progress);
+        iduser = getIntent().getStringExtra("key");
+        DownloadTask task = new DownloadTask();
 
+        /*try {
 
+            JSONArray challengesArray = task.execute("http://35.176.222.11:5000/challenges/").get();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+
+                for(int i = 0; i < challengesArray.length(); i++)
+                {
+                    JSONObject jsonPart = challengesArray.getJSONObject(i);
+
+                    RelativeLayout rl = new RelativeLayout(getBaseContext());
+                    rl.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                    ImageView imgTrophy = new ImageView(getBaseContext());
+                    imgTrophy.setImageDrawable(getDrawable(R.drawable.trophy));
+                    imgTrophy.setLayoutParams(new RelativeLayout.LayoutParams(convertDpToPixel(100, getBaseContext()), convertDpToPixel(100, getBaseContext())));
+                    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                    params.addRule(RelativeLayout.ALIGN_RIGHT);
+                    rl.addView(imgTrophy);
+
+                    TextView textLocation = new TextView(getBaseContext());
+                    RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                    params2.addRule(RelativeLayout.CENTER_IN_PARENT);
+                    textLocation.setLayoutParams(params2);
+                    textLocation.setText(jsonPart.getString("chall_coord"));
+                    rl.addView(textLocation);
+
+                    TextView textKm = new TextView(getBaseContext());
+                    RelativeLayout.LayoutParams params3 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                    params3.addRule(RelativeLayout.CENTER_IN_PARENT);
+                    textKm.setLayoutParams(params3);
+                    textKm.setText(jsonPart.getString("chall_totalKm"));
+                    rl.addView(textKm);
+
+                    TextView textPoints = new TextView(getBaseContext());
+                    RelativeLayout.LayoutParams params4 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                    params4.addRule(RelativeLayout.CENTER_IN_PARENT);
+                    textPoints.setLayoutParams(params4);
+                    textPoints.setText(jsonPart.getString("chall_points"));
+                    rl.addView(textPoints);
+
+                    ImageView imgDone = new ImageView(getBaseContext());
+                    imgDone.setImageDrawable(getDrawable(R.drawable.done));
+                    imgDone.setLayoutParams(new RelativeLayout.LayoutParams(convertDpToPixel(100, getBaseContext()), convertDpToPixel(100, getBaseContext())));
+                    RelativeLayout.LayoutParams params5 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                    params5.addRule(RelativeLayout.ALIGN_RIGHT);
+                    rl.addView(imgDone);
+                }
+            }
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }*/
+    }
+
+    public static int convertDpToPixel(int dp, Context context){
+        return dp * ((int) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
     }
 }
